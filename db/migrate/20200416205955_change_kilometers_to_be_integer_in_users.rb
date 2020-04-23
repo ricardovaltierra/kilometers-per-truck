d80db560659078a73735edf366a23c3b375f188d
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-04-23 06:13:32 -0500

    heroku db-pg fix

--- db/migrate/20200416205955_change_kilometers_to_be_integer_in_users.rb
+++ db/migrate/20200416205955_change_kilometers_to_be_integer_in_users.rb
@@ -1,5 +1,5 @@
 class ChangeKilometersToBeIntegerInUsers < ActiveRecord::Migration[5.2]
   def change
-    change_column :travels, :kilometers, :integer USING kilometers::integer
+    change_column :travels, :kilometers, 'integer USING CAST(kilometers AS integer)'
   end
 end
