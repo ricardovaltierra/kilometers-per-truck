ed1fe124c2f535840ea03a515c88a4d156680250
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-04-29 02:39:44 -0500

    refactored index method on travels controller for views

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -1,11 +1,12 @@
 class TravelsController < ApplicationController
-  def index
-    # @travels = Travel.get_all_unofficial_travels(current_user)
-    # For creating views for My travels and unofficial travel s
-    # one must pass here the current user and the propper scope
-    # to create a conditional but redirect to the same pae
-    # index
-    @travels = Travel.all
+  def index        
+    if params[:travel][:official] == 'true'
+      @travels = Travel.get_all_official(current_user)
+      session[:official] = 'true'
+    else
+      @travels = Travel.get_all_unofficial(current_user)
+      session[:official] = 'false'
+    end    
   end
 
   def new
@@ -16,7 +17,7 @@ class TravelsController < ApplicationController
     @travel = current_user.travels.build(travel_params)
     if @travel.save
       flash[:success] = 'Travel created successfully!'
-      redirect_to travels_path
+      redirect_to travels_path(:travel => { :official => session[:official] })
     else
       flash.now[:warning] = 'Something went wrong. Please try again.'
       render new_travel_path
@@ -30,6 +31,6 @@ class TravelsController < ApplicationController
   private
 
   def travel_params
-    params.require(:travel).permit(:name, :kilometers, :group_id)
+    params.require(:travel).permit(:name, :kilometers, :group_id, :official)
   end
 end
