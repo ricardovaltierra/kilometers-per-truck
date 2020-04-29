20b68a593767ca3fd8e98bb36d51a2905dd04f6c
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-04-28 23:39:11 -0500

    created methods for scoping travels

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -6,11 +6,16 @@ class Travel < ApplicationRecord
 
   default_scope -> { order(created_at: :desc) }
 
-  def self.get_all_unofficial_travels(current_user)
-    all.where("user_id = #{current_user.id} AND group_id != null").order(created_at: :desc)
+  def self.get_all_unofficial(current_user)
+    all_mine(current_user).where(group_id: nil)
+    # .order(created_at: :desc)
   end
 
-  def self.get_all_official_travels(current_user)
-    all.where(user_id: current_user && !group_id.nil?).order(created_at: :desc)
+  def self.get_all_official(current_user)
+    all_mine(current_user).where.not(group_id: nil)
+  end
+
+  def self.all_mine(current_user)
+    all.where(user_id: current_user.id)
   end
 end
