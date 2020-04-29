6b45e1d7b275f3c57a427304a026580344089c60
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-04-29 05:54:39 -0500

    split conditional for index views rescuing nil

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -1,12 +1,21 @@
 class TravelsController < ApplicationController
-  def index        
-    if params[:travel][:official] == 'true'
-      @travels = Travel.get_all_official(current_user)
-      session[:official] = 'true'
+  def index
+    official = params.dig(:travel, :official)
+    if official
+      if params[:travel][:official] == 'true'
+        @travels = Travel.get_all_official(current_user)
+        session[:official] = 'true'
+      else
+        @travels = Travel.get_all_unofficial(current_user)
+        session[:official] = 'false'
+      end
     else
-      @travels = Travel.get_all_unofficial(current_user)
-      session[:official] = 'false'
-    end    
+      if session[:official] == 'true'
+        @travels = Travel.get_all_official(current_user)        
+      else
+        @travels = Travel.get_all_unofficial(current_user)        
+      end
+    end
   end
 
   def new
@@ -17,9 +26,9 @@ class TravelsController < ApplicationController
     @travel = current_user.travels.build(travel_params)
     if @travel.save
       flash[:success] = 'Travel created successfully!'
-      redirect_to travels_path(:travel => { :official => session[:official] })
+      redirect_to travels_path(:travel => { :official => session[:official] })      
     else
-      flash.now[:warning] = 'Something went wrong. Please try again.'
+      flash.now[:warning] = "#{@travel.errors.full_messages.to_sentence}. Please try again."
       render new_travel_path
     end
   end
